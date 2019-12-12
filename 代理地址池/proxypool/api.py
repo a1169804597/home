f836411b5d6beefd035820de507623ce3321718c
wzb <1169804597@qq.com>
2019-12-12 08:04:57 +0800

    代理地址池

---
+++ 代理地址池/proxypool/api.py
@@ -0,0 +1,46 @@
+#接口函数，127.0.0.1:5555
+#/Welcome to Proxy Pool System
+#/random 随机产生代理
+#/count 将获取代理池的容量
+from flask import Flask, g
+
+from .db import RedisClient
+
+__all__ = ['app']
+
+app = Flask(__name__)
+
+
+def get_conn():
+    if not hasattr(g, 'redis'):
+        g.redis = RedisClient()
+    return g.redis  #代理是从redis数据库中获取的
+
+
+@app.route('/')
+def index():
+    return '<h2>Welcome to Proxy Pool System</h2>'
+
+
+@app.route('/random')
+def get_proxy():
+    """
+    Get a proxy
+    :return: 随机代理
+    """
+    conn = get_conn()
+    return conn.random()
+
+
+@app.route('/count')
+def get_counts():
+    """
+    Get the count of proxies
+    :return: 代理池总量
+    """
+    conn = get_conn()
+    return str(conn.count())
+
+
+if __name__ == '__main__':
+    app.run()
