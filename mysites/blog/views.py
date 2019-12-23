c187aaec1de2e3475cbb84c2275a81a89ad43ea5
wzb <1169804597@qq.com>
2019-12-23 21:38:49 +0800

    博客分类统计

--- mysites/blog/views.py
+++ mysites/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response,get_object_or_404
 from django.core.paginator import Paginator
 from django.conf import settings
+from django.db.models import Count
 from . models import Blog,BlogType
 
 # Create your views here.
@@ -22,13 +23,29 @@ def get_blog_list_common_data(request,blogs_all_list):
         page_range.insert(0,1)
     if page_range[-1] !=paginator.num_pages:
         page_range.append(paginator.num_pages)
-
+    #获取博客分类的对应博客数量方法
+    '''方法1
+    blog_types=BlogType.objects.all()
+    blog_types_list=[]
+    for blog_type in blog_types:
+        blog_type.blog_count=Blog.objects.filter(blog_type=blog_type).count()
+        blog_types_list.append(blog_type)
+   方法2
+    BlogType.objects.annotate(blog_count=Count('blog'))
+    '''
+    #获取日期归档对应博客数量
+    blog_dates=Blog.objects.dates('created_time', 'month', order="DESC") # 按创建时间的倒序分类，按月筛选，返回月份的排序如2019年12月
+    blog_dates_dict={}
+    for blog_date in blog_dates:
+        blog_count=Blog.objects.filter(created_time__year=blog_date.year,
+                                       created_time__month=blog_date.month).count()
+        blog_dates_dict[blog_date]=blog_count
     context={}
     context['blogs']=page_of_blogs.object_list          #所有的博客
     context['page_of_blogs']=page_of_blogs              #当前页的对象
     context['page_range'] = page_range                  # 当前页和前后各2页
-    context['blog_types'] = BlogType.objects.all()      # 博客所有的分类
-    context['blog_dates'] = Blog.objects.dates('created_time', 'month', order='DESC')  # 按创建时间的倒序分类，按月筛选，返回月份的排序如2019年12月
+    context['blog_types'] =BlogType.objects.annotate(blog_count=Count('blog'))                # 博客所有的分类
+    context['blog_dates'] =blog_dates_dict
     return context
 
 def blog_list(request):
