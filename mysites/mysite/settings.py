62ffed49275448c7287e45f78e9ab0d690473a61
wzb <1169804597@qq.com>
2019-12-23 22:52:14 +0800

    博客后台富文本编辑

--- mysites/mysite/settings.py
+++ mysites/mysite/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'ckeditor',
+    'ckeditor_uploader',
 ]
 
 MIDDLEWARE = [
@@ -108,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'zh-Hans'
+LANGUAGE_CODE = 'zh-hans'
 
 TIME_ZONE = 'UTC'
 
@@ -126,6 +128,13 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static'),
 ]
+
+# media
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+####配置ckeditor
+#上传路径
+CKEDITOR_UPLOAD_PATH='upload/'
 
 #自定义的参数
 EACH_PAGE_BLOGS_NUMBER=7
