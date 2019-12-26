aaa8ebda62038486c8c0fc4beb922fbb54bb41fd
wzb <1169804597@qq.com>
2019-12-26 22:42:16 +0800

    阅读计数的统计和选择

--- mysites/read_statistics/models.py
+++ mysites/read_statistics/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.fields import exceptions
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.utils import timezone
 
 # Create your models here.
 class ReadNum(models.Model):
@@ -21,3 +22,11 @@ class ReadNumExpandMethod():
             return readnum.read_num
         except exceptions.ObjectDoesNotExist:
             return 0
+
+# 阅读详细
+class ReadDetail(models.Model):
+    date=models.DateField(default=timezone.now)
+    read_num = models.IntegerField(default=0)
+    content_type = models.ForeignKey(ContentType, on_delete=models.DO_NOTHING)  # content_type是模型的名称如blog
+    object_id = models.PositiveIntegerField() # 博客的ID
+    content_object = GenericForeignKey('content_type', 'object_id')   # 是把博客的阅读数量和模型封装的对象
--- mysites/read_statistics/utils.py
+++ mysites/read_statistics/utils.py
@@ -1,17 +1,52 @@
 #@功能：
+import datetime
 from django.contrib.contenttypes.models import ContentType
-from read_statistics.models import ReadNum
+from django.utils import timezone
+from django.db.models import Sum
+from read_statistics.models import ReadNum,ReadDetail
+
 
 def read_statistics_one_read(request,obj):
     ct = ContentType.objects.get_for_model(obj)  # 得到模型对象
     key="%s_%s_read"%(ct.model,obj.pk)           #创建Cookies的键
+    #阅读判断
+
     if not request.COOKIES.get(key):
+        '''
         if ReadNum.objects.filter(content_type=ct, object_id=obj.pk).count():
             # 存在记录
             readnum = ReadNum.objects.get(content_type=ct, object_id=obj.pk)  # 得到阅读集合对象
             # 不存在
         else:
-            readnum = ReadNum(content_type=ct, object_id=obj.pk)  # 不存在得到的是原始的博客计数对象
+            readnum = ReadNum(content_type=ct, object_id=obj.pk)  # 不存在实例化得到的是原始的博客计数对象
+        '''
+        readnum,created=ReadNum.objects.get_or_create(content_type=ct, object_id=obj.pk)
+        #总数据
         readnum.read_num += 1
         readnum.save()
+    #阅读明细判断
+    date=timezone.now().date()
+    '''
+    if ReadDetail.objects.filter(content_type=ct, object_id=obj.pk,date=date).count():
+        readDetail=ReadDetail.objects.get(content_type=ct, object_id=obj.pk,date=date)
+    else:
+        readDetail=ReadDetail(content_type=ct, object_id=obj.pk,date=date)
+    '''
+    readDetail,created=ReadDetail.objects.get_or_create(content_type=ct, object_id=obj.pk,date=date)
+    readDetail.read_num+=1
+    readDetail.save()
     return key
+
+#前七天的阅读明细
+def get_seven_days_read_data(content_type):
+    today=timezone.now().date()
+    dates=[]
+    read_nums=[]
+    for i in range(7,0,-1):
+        date=today-datetime.timedelta(days=i)
+        dates.append(date.strftime('%m/%d'))
+        read_details=ReadDetail.objects.filter(content_type=content_type,date=date)
+        result=read_details.aggregate(read_num_sum=Sum('read_num'))
+        read_nums.append(result['read_num_sum'] or 0)
+    return read_nums ,dates
+
